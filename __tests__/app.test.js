0b1fe6eeabd9064a980022c1aff353b7c5637700
aidanliddiard <aidanliddiard@gmail.com>
2022-06-07 10:43:02 -0700

    refactored test and test passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,15 +2,16 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const {zodiacs} = require('../data/zodacs');
+const { zodiacs } = require('../data/zodiacs');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('/zodiacs should return a list of zodiac signs', async () => {
-    const data = await request(app).get('/zodiacs');
-    expect(data.body).toEqual(zodiacs);
+  it('/zodiac should return a list of zodiac signs', async () => {
+    const data = await request(app).get('/zodiac');
+    const exp = zodiacs.map((zodiac) => ({ id: zodiac.id, name: zodiac.name }));
+    expect(data.body).toEqual(exp);
   });
   afterAll(() => {
     pool.end();
