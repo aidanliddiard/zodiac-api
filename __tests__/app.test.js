8853565e7f9ea1d3d1d10c51999a043940f7be5d
aidanliddiard <aidanliddiard@gmail.com>
2022-06-07 10:47:05 -0700

    wrote test for detail page

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,6 +8,16 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+  it('/zodiac/id should return a detail page of a zodiac sign', async () => {
+    const data = await request(app).get('/zodiac/1');
+    expect(data.body).toEqual({
+      id: '1',
+      name: 'aquarius',
+      dates: 'Jan 21 - Feb 19',
+      symbol: 'Water Bearer',
+    });
+  });
+
   it('/zodiac should return a list of zodiac signs', async () => {
     const data = await request(app).get('/zodiac');
     const exp = zodiacs.map((zodiac) => ({ id: zodiac.id, name: zodiac.name }));
