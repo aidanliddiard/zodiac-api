e2f647fee263629dd3cd1a1906dbf630c747a1b9
aidanliddiard <aidanliddiard@gmail.com>
2022-06-07 09:49:30 -0700

    test written but failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,13 +2,15 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const {zodiacs} = require('../data/zodacs');
 
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('/zodiacs should return a list of zodiac signs', async () => {
+    const data = await request(app).get('/zodiacs');
+    expect(data.body).toEqual(zodiacs);
   });
   afterAll(() => {
     pool.end();
